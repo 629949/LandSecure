5165a253b4cacb6715319e6148644089bcf39ac2
ENG-MARTIN <martinsseba3@gmail.com>
2025-11-27 01:26:22 +0300

    login_signup

--- UserModel.cs
+++ UserModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LandSecure
+﻿namespace LandSecure
 {
-    public class UserModel
+    internal class UserModel
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
